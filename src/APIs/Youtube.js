984e27928969fadc22cee07d76a7dd8d0f6fe3e8
ahsan <unknownuser34754@gmail.com>
2020-10-29 23:46:24 +0500

    youtube browser done

--- src/APIs/Youtube.js
+++ src/APIs/Youtube.js
@@ -1,5 +1,5 @@
 import axios from "axios"
-const KEY = "AIzaSyDpDXJPgL3ZTlaBuIp8vR7lh3KpU42r7Ho"
+//const KEY = "AIzaSyDpDXJPgL3ZTlaBuIp8vR7lh3KpU42r7Ho"
 
 export default axios.create({
     baseURL: "https://www.googleapis.com/youtube/v3"
--- src/components/App.js
+++ src/components/App.js
@@ -1,12 +1,19 @@
 import React, { Component } from 'react'
 import SearchBar from './SearchBar'
 import Youtube from "../APIs/Youtube"
+import VideoList from './VideoList'
+import VideoDetail from './VideoDetail'
+
 
 export class App extends Component {
+    state = { videos: [],selectedVideo:null }
     KEY = "AIzaSyDpDXJPgL3ZTlaBuIp8vR7lh3KpU42r7Ho"
+    onVideoSelect = video =>{
+        this.setState({selectedVideo:video})
+    }
 
-    onTermSubmit = term => {
-        Youtube.get('/search', {
+    onTermSubmit = async term => {
+        const response = await Youtube.get('/search', {
             params: {
                 q: term,
                 key: this.KEY,
@@ -15,12 +22,21 @@ export class App extends Component {
                 part: 'snippet'
             }
         })
+        this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] })
 
     }
     render() {
         return (
             <div className="ui container">
                 <SearchBar onFormSubmit={this.onTermSubmit} />
+                <div className="ui grid">
+                    <div className="ui row">
+                    <div className="eleven wide column">
+                <VideoDetail video={this.state.selectedVideo}/></div>
+                <div className="five wide column">
+                <VideoList onVideoSelect={this.onVideoSelect} videos={this.state.videos}></VideoList></div>
+                </div>
+                </div>
             </div>
         )
     }
